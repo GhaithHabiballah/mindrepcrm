eed10adcf5af579c80c3d3496485dd16b64be792
RomirJ <romirj@gmail.com>
2026-02-06 21:00:09 -0800

    Fix glide css import for build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@glideapps/glide-data-grid/dist/index.css';
-import '@glideapps/glide-data-grid/dist/extra.css';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
 
